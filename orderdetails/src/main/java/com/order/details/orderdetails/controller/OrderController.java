777486a1a644232f05eb41bff787c55838fcb079
nuDeveloper <anandkumarsharma005@gmail.com>
2022-02-12 21:39:26 +0530

    Feign Client demo

---
+++ orderdetails/src/main/java/com/order/details/orderdetails/controller/OrderController.java
@@ -0,0 +1,45 @@
+package com.order.details.orderdetails.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.order.details.orderdetails.entity.Order;
+import com.order.details.orderdetails.service.OrderService;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+	@Autowired
+	OrderService orderService;
+
+	@GetMapping("/get")
+	public Map<Integer, List<Order>> getAll() {
+		return orderService.getAll();
+	}
+
+	@GetMapping("/get/{userId}")
+	public List<Order> getById(@PathVariable("userId") Integer userId) {
+		return orderService.getById(userId);
+	}
+
+	@PostMapping("/post/{userId}")
+	public List<Order> postByVariable(@PathVariable("userId") Integer userId, @RequestBody List<Order> orderList) {
+		return orderService.postByVariable(userId, orderList);
+	}
+
+	@PostMapping("/post")
+	public List<Order> postByParam(@RequestParam("userId") Integer userId, @RequestBody List<Order> orderList) {
+		return orderService.postByParam(userId, orderList);
+	}
+
+}
